c57504b1c42fc671589046cf2a0c89d1a565e4a7
Thomas Pelletier <thomas@pelletier.codes>
2021-09-13 22:35:11 -0400

    document: 80% API proposal

--- x/document/document.go
+++ x/document/document.go
@@ -1,56 +1,232 @@
+// Package document provides tools for manipulating the structure of TOML
+// documents.
+//
+// While github.com/pelletier/go-toml provides efficient functions to transform
+// TOML documents to and from usual Go types, this package allows you to create
+// and modify the structure of a TOML document.
+//
+// Design decisions
+//
+// Document does not represent white space. When parsing a document from bytes,
+// all white space is discarded. The only control over white space is provided
+// by the document encoder in the form of general rules. One use case that is
+// not covered is modifying an existing TOML document, while keeping the
+// non-modified part of the document exactly the same byte-for-byte. However it
+// simplifies the API and parsing significantly.
+//
+// It is a design goal to be able to write literal Documents and modify them
+// without too much assistance. For example, instead of providing dozens of
+// Create / Modify / Delete functions for all kinds of nodes, the current design
+// provides allows the user to manipulate pointers and slices like any other Go
+// data structure. The drawback is that the operations performed on the Document
+// cannot be validated immediately. A certain amount of constraint is added in
+// the form of typing, but ultimately it is the responsibility of the user to
+// call Valid() after reading or before writing a Document, if they wishes to
+// only deal with valid documents.
+//
+// While many operations would feel natural on maps, this Document structure
+// actually only contains slices of elements to represent parent / children
+// relationships. This allows the user to completely control the ordering of
+// their document, as well as its exact shape. For example, the following valid
+// documents can all be represented:
+//
+//  a.b.c = 42
+//
+//  [a]
+//  b.c = 42
+//
+//  [a.b]
+//  c = 42
+//
+//  [a]
+//  b = { c = 42 }
+//
+//  [a]
+//  [a.b]
+//  c = 42
+//
+//  [a.b]
+//  c = 42
+//  [a]
+//
+// Comments are a first class object in this model. An often requested feature
+// is to preserve and manipulate comments in TOML documents. By embedding them
+// in the core of every node, full control is provided to the user on how they
+// want to comment their document.
+//
+// See the Examples for examples of classic Document usages.
 package document
 
 import (
-	"fmt"
-
-	"github.com/pelletier/go-toml/v2/internal/ast"
-	"github.com/pelletier/go-toml/v2/internal/parser"
+	"strconv"
 )
 
 // Document represents a TOML document.
-// It is not guaranteed to be a valid document when manually modified.
-// Has helper functions to perform usual operations.
 type Document struct {
-	Root []Node
+	KeyValues []*KeyValue
+	Tables    []*Table
+
+	// Optional last comment of the document.
+	TrailerComment Comment
 }
 
-type Node struct {
-	Kind     ast.Kind
-	Comment  string
-	Value    interface{}
-	Children []Node
-	Flags    int
+// GetAt traverses the document to return a pointer to the Value stored at the
+// path represented by parts. Returns nil if no such document exists.
+//
+// Even though part/s is of type interface{}, each of them should be either a
+// string or an int. If it is a string, it is interpreted as a table or
+// key-value key part. If it is an integer, it is interpreted as an array index.
+// -1 is used to denote the last element of the array, if it exists. Any other
+// type panics.
+//
+// This function operates on the structure of the document. If the path is not
+// explicitly defined in the document this function returns nil.
+func (d Document) GetAt(part interface{}, parts ...interface{}) Value {
+	// TODO
+	return nil
 }
 
-func Parse(b []byte) (Document, error) {
-	d := Document{}
+// ParentOf returns the immediate parent of a given Value. Panics if the parent
+// does not exist. A classic use-case is to first call GetAt to retrieve a
+// specific element, then call ParentOf to get the parent and possibly reorder
+// or delete the element.
+func (d Document) ParentOf(v Value) Value {
+	// TODO
+	return nil
+}
 
-	p := parser.Parser{}
-	p.Reset(b)
-	p.Comments = true
+// Valid verifies that the document is fully compliant with the TOML
+// specification. It returns nil if it is valid, or a list of errors otherwise.
+// While this function tries to find all errors, it does not guarantee to find
+// them all if at least one error is found.
+func (d Document) Valid() []error {
+	// TODO
+	return nil
+}
 
-	for p.NextExpression() {
-		expr := p.Expression()
-		node, err := exprToRootNode(expr)
-		if err != nil {
-			return d, err
-		}
-		d.Root = append(d.Root, node)
+type Key []KeyPart
+
+type KeyKind int
+
+const (
+	BareKey KeyKind = iota
+	LiteralKey
+	QuotedKey
+)
+
+// StringKey is a convenience function to generate a Key from strings. It is
+// mostly useful when expressing documents as literals.
+// The kind precedence of each part is BareKey > LiteralKey > QuotedKey.
+func StringKey(part1 string, parts ...string) Key {
+	// TODO
+	return Key{}
+}
+
+type StringKind int
+
+const (
+	BasicString StringKind = 1 << iota
+	LiteralString
+	MultilineString
+)
+
+type KeyPart struct {
+	Value string
+	Kind  KeyKind
+}
+
+type KeyValue struct {
+	Comment   Comment
+	Commented bool
+
+	Key   Key
+	Value Value
+}
+
+// Value is an interface supported by all the terminal types of a TOML document.
+type Value interface {
+	isValue()
+}
+
+type String struct {
+	Value string
+	Kind  StringKind
+}
+
+func (s *String) isValue() {}
+
+type Integer struct {
+	V string
+}
+
+func (i *Integer) isValue() {}
+
+func (i *Integer) Set(v int64) {
+	i.V = strconv.FormatInt(v, 10)
+}
+
+func (i *Integer) FromString(v string) {
+	i.V = v
+}
+
+func (i Integer) Value() int64 {
+	v, err := strconv.ParseInt(i.V, 10, 64)
+	if err != nil {
+		panic("document should not let an invalid integer be stored")
 	}
-
-	return d, p.Error()
+	return v
 }
 
-func exprToRootNode(expr *ast.Node) (Node, error) {
-	switch expr.Kind {
-	case ast.Table:
-		panic("todo")
-	case ast.ArrayTable:
-		panic("todo")
-	case ast.KeyValue:
-		panic("todo")
-	default:
-		// TODO: add error context
-		return Node{}, fmt.Errorf("expression of type '%s' not allowed there", expr.Kind)
-	}
+func (i Integer) String() string {
+	return i.V
 }
+
+type Boolean struct {
+	V bool
+}
+
+func (b *Boolean) isValue() {}
+
+// TODO: Float should be the same as Integer
+// TODO: different types of dates should follow the same model.
+
+type Array struct {
+	Comment   Comment
+	Commented bool
+
+	// Should each element of the array be on its own line.
+	Multiline bool
+	Elements  []Value
+}
+
+func (a *Array) isValue() {}
+
+type InlineTable struct {
+	Elements []*KeyValue
+}
+
+type Table struct {
+	Comment   Comment
+	Commented bool
+	Array     bool
+
+	Key      Key
+	Elements []*KeyValue
+}
+
+// Comment is usually a member of an element of the TOML document.
+type Comment struct {
+	// Can be multiline. An empty value means no comment.
+	Value string
+	// Inline puts the comment on the same line as the element it annotates
+	// (by default comments are above their element).
+	Inline bool
+}
+
+// Notes / TODOs:
+// - Array Table may be difficult to manipulate?
+// - Use *As* functions to easily write literals (and panic if construction is
+// - incorrect).
+// - Use *To* functions to convert to/from standard types, returning errors if
+//   needed.
+// - How to discover the structure?
--- x/document/document_test.go
+++ x/document/document_test.go
@@ -1,142 +1,236 @@
 package document_test
 
 import (
-	"testing"
+	"fmt"
 
 	"github.com/pelletier/go-toml/v2/x/document"
-	"github.com/stretchr/testify/require"
 )
 
-func doc(entities ...document.Entity) document.Document {
-	return document.Document{
-		document.Container{
-			Elements: entities,
-		},
-	}
-}
-
-func key(k string) document.Key {
-	return document.Key{
-		Name: k,
-	}
-}
-
-func TestDocument(t *testing.T) {
-	examples := []struct {
-		name string
-		toml string
-		doc  document.Document
-		err  error
-	}{
-		{
-			name: "assign decimal int",
-			toml: `x = 42`,
-			doc: doc(
-				&document.KeyValue{
-					Key:   key("x"),
-					Value: &document.Integer{Value: 42},
-				},
-			),
-			err: nil,
-		},
-		{
-			name: "assign string",
-			toml: `x = "hello"`,
-			doc: doc(
-				&document.KeyValue{
-					Key: key("x"),
-					Value: &document.String{
-						Value: "hello",
+func ExampleDocument_getAt() {
+	doc := document.Document{
+		KeyValues: []*document.KeyValue{
+			{
+				Key: document.StringKey("array"),
+				Value: &document.Array{
+					Elements: []document.Value{
+						&document.String{Value: "zero"},
+						&document.String{Value: "one"},
+						&document.String{Value: "two"},
 					},
 				},
-			),
-			err: nil,
+			},
 		},
-		{
-			name: "assign string and int",
-			toml: `a = "hello"
-b = 42`,
-			doc: doc(
-				&document.KeyValue{
-					Key:   key("a"),
-					Value: &document.String{Value: "hello"},
+		Tables: []*document.Table{
+			{
+				Key: document.StringKey("a", "b"),
+				Elements: []*document.KeyValue{
+					{
+						Key:   document.StringKey("c"),
+						Value: &document.String{Value: "value"},
+					},
 				},
-				&document.KeyValue{
-					Key:   key("b"),
-					Value: &document.Integer{Value: 42},
-				},
-			),
-			err: nil,
-		},
-		{
-			name: "table",
-			toml: `[a]`,
-			doc: doc(
-				&document.Table{
-					Key: key("a"),
-				},
-			),
-			err: nil,
-		},
-		{
-			name: "table with one assign",
-			toml: `[a]
-b = 1`,
-			doc: doc(
-				&document.Table{
-					Key: key("a"),
-				},
-				&document.KeyValue{
-					Key:   key("b"),
-					Value: &document.Integer{Value: 1},
-				},
-			),
-		},
-		{
-			name: "table with two assigns",
-			toml: `
-[a]
-b = 1
-c = 2`,
-			doc: doc(
-				&document.Table{
-					Key: key("a"),
-				},
-				&document.KeyValue{
-					Key:   key("b"),
-					Value: &document.Integer{Value: 1},
-				},
-				&document.KeyValue{
-					Key:   key("c"),
-					Value: &document.Integer{Value: 2},
-				},
-			),
-		},
-		{
-			name: "table with implicit intermediate",
-			toml: `[a.b]
-		c = 1`,
-			doc: doc(
-				&document.Table{
-					Key: key("a"),
-				},
-				&document.KeyValue{
-					Key:   key("c"),
-					Value: &document.Integer{Value: 1},
-				},
-			),
-			err: nil,
+			},
 		},
 	}
 
-	for _, e := range examples {
-		t.Run(e.name, func(t *testing.T) {
-			d, err := document.Parse([]byte(e.toml))
-			if e.err != nil {
-				require.Equal(t, e.err, err)
-			} else {
-				require.Equal(t, e.doc, d)
-			}
-		})
+	// Can retrieve explicit tables.
+	fmt.Println("table:", doc.GetAt("a", "b"))
+	// Can retrieve leaf nodes.
+	fmt.Println("leaf:", doc.GetAt("a", "b", "c"))
+	// Returns nil for nonexistent nodes.
+	fmt.Println("nonexistent:", doc.GetAt("doesnotexist"))
+	// Does not retrieve implicit tables.
+	fmt.Println("implicit:", doc.GetAt("a"))
+	// Can use index to get inside an array.
+	fmt.Println("index:", doc.GetAt("array", 1))
+	// Index outside of the range of an array returns nil.
+	fmt.Println("oob:", doc.GetAt("array", 42))
+	// Index can be -1 to mean the last element of the array.
+	fmt.Println("last:", doc.GetAt("array", -1))
+
+	// Output:
+	// table: {{"a", "b"}, {"c", "value}}
+	// leaf: {"value"}
+	// nonexistent: nil
+	// implicit: nil
+	// index: {"one"}
+	// oob: nil
+	// last: {"two"}
+
+}
+
+func ExampleDocument_arrayTable() {
+	doc := document.Document{
+		Tables: []*document.Table{
+			{
+				Array: true,
+				Key:   document.StringKey("products"),
+				Elements: []*document.KeyValue{
+					{
+						Key:   document.StringKey("name"),
+						Value: &document.String{Value: "Hammer"},
+					},
+					{
+						Key:   document.StringKey("sku"),
+						Value: &document.Integer{V: "738594937"},
+					},
+				},
+			},
+			{
+				Array: true,
+				Key:   document.StringKey("products"),
+				Comment: document.Comment{
+					Value:  "empty table within the array",
+					Inline: true,
+				},
+			},
+			{
+				Array: true,
+				Key:   document.StringKey("products"),
+				Elements: []*document.KeyValue{
+					{
+						Key:   document.StringKey("name"),
+						Value: &document.String{Value: "Nail"},
+					},
+					{
+						Key:   document.StringKey("sku"),
+						Value: &document.Integer{V: "284758393"},
+					},
+					{
+						Key:   document.StringKey("color"),
+						Value: &document.String{Value: "gray"},
+					},
+				},
+			},
+		},
 	}
+
+	fmt.Printf("%+v", doc)
+
+	// Output:
+	// [[products]]
+	// name = "Hammer"
+	// sku = 738594937
+	//
+	// [[products]] # empty table within the array
+	//
+	// [[products]]
+	// name = "Nail"
+	// sku = 284758393
+	// color = "gray"
+}
+
+func ExampleDocument_reference() {
+	doc := document.Document{
+		KeyValues: []*document.KeyValue{
+			{
+				Key:   document.StringKey("title"),
+				Value: &document.String{Value: "TOML Example"},
+			},
+		},
+		Tables: []*document.Table{
+			{
+				Key: document.StringKey("owner"),
+				Elements: []*document.KeyValue{
+					{
+						Key:   document.StringKey("name"),
+						Value: &document.String{Value: "Tom Preston-Werner"},
+					},
+					// TODO: dob
+				},
+			},
+			{
+				Key: document.StringKey("database"),
+				Elements: []*document.KeyValue{
+					{
+						Key:   document.StringKey("enabled"),
+						Value: &document.Boolean{V: true},
+					},
+					{
+						Key: document.StringKey("ports"),
+						Value: &document.Array{
+							Elements: []document.Value{
+								&document.Integer{V: "8000"},
+								&document.Integer{V: "8001"},
+								&document.Integer{V: "8002"},
+							},
+						},
+					},
+					{
+						Key: document.StringKey("data"),
+						Value: &document.Array{
+							Elements: []document.Value{
+								&document.Array{
+									Elements: []document.Value{
+										&document.String{Value: "delta"},
+										&document.String{Value: "phi"},
+									},
+								},
+								&document.Array{
+									Elements: []document.Value{
+										// TODO floats
+										// document.Float{V: "3.14"},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			{
+				Key: document.StringKey("servers"),
+			},
+			{
+				Key: document.StringKey("servers", "alpha"),
+				Elements: []*document.KeyValue{
+					{
+						Key:   document.StringKey("ip"),
+						Value: &document.String{Value: "127.0.0.1"},
+					},
+					{
+						Key:   document.StringKey("role"),
+						Value: &document.String{Value: "frontend"},
+					},
+				},
+			},
+			{
+				Key: document.StringKey("servers", "beta"),
+				Elements: []*document.KeyValue{
+					{
+						Key:   document.StringKey("ip"),
+						Value: &document.String{Value: "127.0.0.2"},
+					},
+					{
+						Key:   document.StringKey("role"),
+						Value: &document.String{Value: "backend"},
+					},
+				},
+			},
+		},
+	}
+
+	fmt.Println(doc)
+
+	// Output:
+	// title = "TOML Example"
+	//
+	// [owner]
+	// name = "Tom Preston-Werner"
+	// dob = 1979-05-27T07:32:00-08:00
+	//
+	// [database]
+	// enabled = true
+	// ports = [ 8000, 8001, 8002 ]
+	// data = [ ["delta", "phi"], [3.14] ]
+	// temp_targets = { cpu = 79.5, case = 72.0 }
+	//
+	// [servers]
+	//
+	// [servers.alpha]
+	// ip = "10.0.0.1"
+	// role = "frontend"
+	//
+	// [servers.beta]
+	// ip = "10.0.0.2"
+	// role = "backend"
 }
